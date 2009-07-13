bd9dd104dffc340df8e404f9de51baf847b34e2f
José Valim <jose.valim@gmail.com>
2009-07-13 17:20:32 +0200

    Skip routes or views specs if desired.

--- lib/generators/rspec/scaffold/scaffold_generator.rb
+++ lib/generators/rspec/scaffold/scaffold_generator.rb
@@ -10,12 +10,20 @@ module Rspec
       class_option :template_engine, :desc => "Template engine to generate view files"
       class_option :singleton, :type => :boolean, :desc => "Supply to create a singleton controller"
 
+      class_option :skip_views_specs,  :type => :boolean,
+                                       :desc => "When supplied skip routes specs"
+
+      class_option :skip_routes_specs, :type => :boolean,
+                                       :desc => "When supplied skip views specs"
+
       def copy_controller_files
         template 'controller_spec.rb',
                  File.join('spec/controllers', controller_class_path, "#{controller_file_name}_controller_spec.rb")
       end
 
       def copy_view_files
+        return if options[:skip_views_specs]
+
         copy_view :edit
         copy_view :index unless options[:singleton]
         copy_view :new
@@ -23,6 +31,8 @@ module Rspec
       end
 
       def copy_routing_files
+        return if options[:skip_routes_specs]
+
         template 'routing_spec.rb',
                  File.join('spec/routing', controller_class_path, "#{controller_file_name}_routing_spec.rb")
       end
