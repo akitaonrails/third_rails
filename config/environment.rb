11862f02173ae1ca5ef9bc29b525db407e9d711b
José Valim <jose.valim@gmail.com>
2009-09-18 08:09:30 -0300

    Updated Rails and Rack::Bug dependencies.

--- config/environment.rb
+++ config/environment.rb
@@ -22,7 +22,7 @@ Rails::Initializer.run do |config|
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
-  config.plugins = [ :orchestra, :all ]
+  # config.plugins = [ :exception_notifier, :all ]
 
   # Skip frameworks you're not going to use. To use Rails without a database,
   # you must remove the Active Record framework.
